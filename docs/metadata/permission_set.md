dc825059a195afba18ace90609a5adcfe25b7b78
殷亮辉 <yinlianghui@hotoa.com>
2020-11-24 18:24:23 +0800

    fix Permission Set

---
+++ docs/metadata/permission_set.md
@@ -0,0 +1,3 @@
+---
+title: Permission Set
+---
--- sidebars.js
+++ sidebars.js
@@ -3,6 +3,6 @@ module.exports = {
     "Getting Started": ['overview', 'installation'],
     "Metadata API": [{
       "Object": ['metadata/object/summary', 'metadata/object/field', 'metadata/object/listview', 'metadata/object/button', 'metadata/object/permission', 'metadata/object/validation_rule', 'metadata/object/workflow_rule']
-    },'metadata/application','metadata/dashboard','metadata/permissionset'],
+    },'metadata/application','metadata/dashboard','metadata/permission_set'],
   },
 };
