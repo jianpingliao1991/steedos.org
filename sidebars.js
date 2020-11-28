334aaca0cb65b7d484608351902631f97f2bec50
殷亮辉 <yinlianghui@hotoa.com>
2020-11-28 15:22:03 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -7,7 +7,7 @@ module.exports = {
         "Field Type": ["metadata/object/field_type/basic", "metadata/object/field_type/complex", "metadata/object/field_type/built_in"]
       }, 
       'metadata/object/listview', 'metadata/object/button', 'metadata/object/permission', 'metadata/object/validation_rule', 'metadata/object/workflow_rule']
-    },'metadata/application','metadata/report','metadata/permission_set'],
+    }, 'metadata/application', 'metadata/report','metadata/permission_set', 'metadata/profile'],
     "Calculate With Formulas":[
       "formula/summary",
       "formula/create", 
